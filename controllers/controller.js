208c7c2192d6c36773e565b0e1c491ff96f32818
Kamil Ladhani <kamilladhani@gmail.com>
2017-02-11 11:45:22 -0800

    Adding notes to app/firebase

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,24 @@
-
 var notesApp = angular.module("notesApp", ['ngRoute']);
 
-notesApp.controller('noteList', function ($scope) {
-	$scope.notes = [
-		{title:'Kamil', value: 'Vancouver'}, 
-		{title:'John', value: 'Whistler'},
-		{title:'Trisha', value: 'Toronto'},
-		{title:'Colleen', value: 'Waterloo'}
-	];
+notesApp.controller('noteList', function ($scope, $timeout) {
+
+	console.log("controller Called");
+
+	$scope.notes = [];
+	var notesRef = firebase.database().ref().child('Notes');
+	notesRef.on("child_added", snap => {
+		$timeout(function() {
+			console.log("listener");
+			var title = snap.child("title").val();
+			var value = snap.child("value").val();
+			$scope.notes.push({title: title, value: value});
+		});
+	});
+
 
 	$scope.addNote = function () {
 
-		var fbRef = firebase.database().ref();
-
+		var fbRef = firebase.database().ref().child("Notes");
 		var title = $scope.newNote.title;
 		var value = $scope.newNote.value;
 
@@ -22,3 +28,4 @@ notesApp.controller('noteList', function ($scope) {
 
 	};
 });
+
